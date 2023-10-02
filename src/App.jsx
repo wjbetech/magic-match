0350c5c9934dbf8c0c844ad2d0b641fe1b2ddb10
William East <wjbetech@gmail.com>
2023-10-02 17:28:46 +0900

    Matching cards loosely set up

--- src/App.jsx
+++ src/App.jsx
@@ -4,55 +4,65 @@ import Card from "./components/Card"
 import { v4 as uuidv4 } from 'uuid'
 
 const cardImages = [
-  { "src": "/assets/helmet.png" },
-  { "src": "/assets/diamond-ring.png" },
-  { "src": "/assets/potion.png" },
-  { "src": "/assets/scroll.png" },
-  { "src": "/assets/shield.png" },
-  { "src": "/assets/sword.png" },
+  { "src": "/assets/helmet.png", matched: false },
+  { "src": "/assets/diamond-ring.png", matched: false },
+  { "src": "/assets/potion.png", matched: false },
+  { "src": "/assets/scroll.png", matched: false },
+  { "src": "/assets/shield.png", matched: false },
+  { "src": "/assets/sword.png", matched: false },
 ]
 
 export default function App() {
-
   // state setters
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
   const [choiceOne, setChoiceOne] = useState(null);
   const [choiceTwo, setChoiceTwo] = useState(null);
+  const [flipState, setFlipState] = useState(false);
+  const [clickable, setClickable] = useState(true)
 
   // useEffect
   useEffect(() => {
     if (choiceOne && choiceTwo) {
-      if (choiceOne.src === choiceTwo.src) {
-        console.log("Match!")
-      } else {
-        console.log("No Match!")
+
+      if (choiceOne || choiceTwo) {
+        setFlipState(true)
       }
-      setChoiceOne("")
-      setChoiceTwo("")
-      console.log("Board reset")
+
+      // map out matched cards
+      if (choiceOne.src === choiceTwo.src) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (card.src === choiceOne.src) {
+              return {...card, matched: true}
+            } else {
+              return card
+            }
+          })
+        })
+      } else {
+        console.log("No Match! Try again")
+        setChoiceOne("")
+        setChoiceTwo("")
+      }
     }
-  }, [choiceTwo])
+  }, [choiceOne, choiceTwo])
 
   // double and shuffle cards
   const cardHandler = () => {
-
     // doouble cards, map over and give random id
     const shuffledCards = [...cardImages, ...cardImages]
       .sort(() => Math.random() - 0.5)
       .map((card) => ({ ...card, id: uuidv4() }))
-
     // put cards into state
     setCards(shuffledCards);  
-
+    setTurns(0)
   }
 
   // handle choices
   const handleChoice = (card) => {
-    
     // update choices
     choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
-
     // increment turns
     setTurns(turns + 1)
   }
@@ -69,7 +79,13 @@ export default function App() {
       {/* generate grid */}
       <div className="card-grid">
         {cards.map((card) => (
-          <Card key={card.id} card={card} handleChoice={handleChoice} />
+          <Card 
+            key={card.id} 
+            card={card} 
+            handleChoice={handleChoice} 
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            clickable={clickable}
+          />
         ))}
       </div>
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Card = ({ card, handleChoice }) => {
+const Card = ({ card, handleChoice, flipped }) => {
 
   const handleClick = () => {
     handleChoice(card)
@@ -8,21 +8,18 @@ const Card = ({ card, handleChoice }) => {
 
   return (
     <div className="card">
-
-        {/* back of card - pattern bg */}
-        <div 
-            className="card-back" 
-            onClick={handleClick}>
-        </div>
-
-        {/* front of card - icon  */}
-        <div className="card-front">
+        {flipped
+        ? <div className="card-front">
             <img 
                 src={card.src} 
                 alt="image card" 
             />
         </div>
-
+        : <div 
+            className="card-back" 
+            onClick={handleClick}>
+        </div>
+        }
     </div>
   )
 }
